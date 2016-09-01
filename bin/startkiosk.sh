bc8d59a1032f2eaea98248f7f5672a805d141a21
cazro <edk@ericdavidking.com>
2016-09-01 15:28:13 -0500

    Changes to be committed: 	typechange: bin/startkiosk.sh
     Changes not staged for commit:
    	modified:   .gitignore
    	typechange: etc/profile.d/kiosk.sh
    
     Untracked files:
    	node_modules/

--- bin/startkiosk.sh
+++ bin/startkiosk.sh
@@ -1,1 +1,18 @@
-/home/pi/startkiosk.sh
+#!/bin/sh
+xset -dpms
+xset s off
+xset s noblank
+x11vnc -display :0 -shared -forever &
+unclutter -d :0  &
+export NODE_ENV=production
+cd /opt/navapp/
+pm2 delete all
+pm2 start /opt/navapp/app.js --name navapp
+echo "Waiting 40 seconds"
+sleep 40s
+echo "Starting matchbox-window-manager"
+matchbox-window-manager &
+echo "Starting Midori"
+while true; do
+   midori -e Fullscreen -a http://localhost
+done
